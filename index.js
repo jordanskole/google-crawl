6509bea2a88eb0be1d526239aaca4a9cbc09e8a6
Jordan Skole <jordan@usebenchmark.com>
2016-07-19 16:49:18 -0400

    add procfile!

--- index.js
+++ index.js
@@ -10,26 +10,30 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 
 var Nightmare = require('nightmare');
 
-
+// build a root route, so that we know stuff is working
 app.get('/', function (req, res) {
+  // and let anyone know everything is a-okay
   res.send('Things are whirring!');
 });
 
 app.post('/search', upload.array(), function (req, res) {
-  console.log('query received');
   // check and make sure that we have received a query to search for
   if ('q' in req.body) {
     // looks like we received a query in the post request.
     // lets build our google search query
     var query = 'https://google.com/search?q=' + req.body.q
 
+    // create a new nightmare
     nightmare = Nightmare();
     // and fire up nightmare
     nightmare
       // .useragent("Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36")
+      // go to the google serp page we created earlier
       .goto(query)
-      .wait('div.rc > h3.r > a')
+      .wait('div.rc > h3.r > a') // for now, keep it simple
       .evaluate(function () {
+        // and only grab the href from the first result
+        // TODO: build this out to include more information from the result
         return document.querySelector('h3.r > a').getAttribute('href');
       })
       .end()
